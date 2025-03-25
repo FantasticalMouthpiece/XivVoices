d07ed8424f68b3e102fca435325b28ff27b473c4
Elaina <github@keifufu.dev>
2025-03-25 19:45:13 +0100

    ffmpeg-wine: support macos
    * don't mess with ffmpeg-wine.sh line endings
    
    * check if process started successfully after every start, not just initially
    
    * my best guess at the macos prefix (might be the same coincidentally)
    
    * provide port to ffmpeg-wine.sh
    
    * more mac fixes
    
    * list _all_ dependencies in /xivv wine
    
    * only start ffmpeg-wine if the setting is actually enabled
    
    * kill is a builtin
    
    * wine registry test fix
    
    * oops
    
    * actually update the registry correctly
    
    * more stuff to test
    
    * flush writer
    
    * oops
    
    * more logging & im dum
    
    * yes
    
    * nc -> ncat as a test
    
    * this might finally work
    
    * nc -> ncat
    
    * oki doki
    
    * oops
    
    * i do not trust your silly PATH

--- Data/ffmpeg-wine.sh
+++ Data/ffmpeg-wine.sh
@@ -1,51 +1,73 @@
 #!/usr/bin/env bash
 
-PORT=6914
+PORT="$1"
 should_exit=0
+[[ -z "$PORT" ]] && exit 1 # exit early, otherwise kill_orphaned_ncat could kill other netcat processes
 
 is_port_in_use() {
-  if netstat -tuln | grep -q ":$1 "; then
-    return 0
-  elif ss -tuln | grep -q ":$1 "; then
-    return 0
+  PORT=$1
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    if netstat -an | grep -q "LISTEN" | grep -q ":$PORT "; then
+      return 0
+    fi
   else
-    return 1
+    if ss -tuln | grep -q ":$PORT "; then
+      return 0
+    fi
   fi
+  return 1
 }
 
 check_dependencies() {
+  # while most of these commands should be preinstalled on any sane distro,
+  # there exists nixos/arch where the users can be... well, users.
+  command -v netstat >/dev/null 2>&1 || command -v ss >/dev/null 2>&1 || should_exit=1
   command -v ffmpeg >/dev/null 2>&1 || should_exit=1
-  command -v nc >/dev/null 2>&1 || should_exit=1
-
-  # We don't support GNU netcat
-  if nc -h 2>&1 | grep -q "GNU"; then
-    should_exit=1
-  fi
+  command -v pgrep >/dev/null 2>&1 || should_exit=1
+  command -v grep >/dev/null 2>&1 || should_exit=1
+  command -v ncat >/dev/null 2>&1 || should_exit=1
+  command -v wc >/dev/null 2>&1 || should_exit=1
 
   if is_port_in_use $PORT; then
+    echo "Port $PORT is already in use"
     should_exit=1
   fi
 }
-check_dependencies
 
-run_command() {
-  local cmd="$1"
-
-  if [[ "$cmd" == "exit" ]]; then
-    should_exit=1
-    return
-  fi
-
-  if [[ "$cmd" =~ ^ffmpeg[[:space:]] ]]; then
-    eval "$cmd" > /dev/null 2>&1
-  fi
-}
-
-while [[ $should_exit -eq 0 ]] ; do
-  { 
-    read -r cmd
-    if [[ -n "$cmd" ]]; then
-      run_command "$cmd"
+kill_orphaned_ncat() {
+  # ncat ocasionally gets orphaned, kill it if there aren't two
+  # instances of ffmpeg-wine.sh (this one and another running nc)
+  instances_of_ffmpeg_wine=$(pgrep -f "ffmpeg-wine.sh" | grep -v $$ | wc -l)
+  if [[ $instances_of_ffmpeg_wine -ne 2 ]]; then
+    ncat_pid=$(pgrep -f "ncat -4 -l 127.0.0.1 -p $PORT")
+    if [[ -n "$ncat_pid" ]]; then
+      kill "$ncat_pid" 2>/dev/null
     fi
-  } < <(nc -l 127.0.0.1 $PORT)
-done
+  fi
+}
+
+main() {
+  check_dependencies
+  kill_orphaned_ncat
+
+  echo "Starting daemon on port $PORT. Will exit immediately if any dependencies are unmet. There will be no logs from here on."
+  while [[ $should_exit -eq 0 ]] ; do
+    ncat -4 -l 127.0.0.1 -p $PORT -c '
+      read -r cmd
+      if [[ "$cmd" == "exit" ]]; then
+        echo "exit" > /tmp/ncat_server_exit
+        exit
+      fi
+      if [[ "$cmd" =~ ^ffmpeg[[:space:]] ]]; then
+        eval "$cmd" > /dev/null 2>&1
+      fi
+    '
+
+    if [[ -f /tmp/ncat_server_exit ]]; then
+      should_exit=1
+      rm /tmp/ncat_server_exit
+    fi
+  done
+}
+
+main
--- XivVoices/PluginWindow.cs
+++ XivVoices/PluginWindow.cs
@@ -989,57 +989,120 @@ public class PluginWindow : Window, IDisposable
             return;
         }
 
-        ImGui.Dummy(new Vector2(0, 10 * ImGuiHelpers.GlobalScale));
-        ImGui.TextWrapped("FFmpeg Settings");
-        ImGui.Dummy(new Vector2(0, 10 * ImGuiHelpers.GlobalScale));
-
-        var wineUseNativeFFmpeg = Plugin.Config.WineUseNativeFFmpeg;
-        if (ImGui.Checkbox("##wineUseNativeFFmpeg", ref wineUseNativeFFmpeg))
+        ImGui.Unindent(8 * ImGuiHelpers.GlobalScale);
+        using (var child = ImRaii.Child("ScrollingRegion", new Vector2(360 * ImGuiHelpers.GlobalScale, -1), false, ImGuiWindowFlags.NoScrollbar))
         {
-            Plugin.Config.WineUseNativeFFmpeg = wineUseNativeFFmpeg;
-            Plugin.Config.Save();
-        }
-        ImGui.SameLine();
-        ImGui.Text("Use native FFmpeg");
-        ImGui.Indent(16 * ImGuiHelpers.GlobalScale);
-        ImGui.Bullet();
-        ImGui.TextWrapped("Increases speed and prevents lag spikes on voices with effects (e.g. Dragons) and when using a playback speed other than 100.");
-        ImGui.Unindent(16 * ImGuiHelpers.GlobalScale);
-        
-        ImGui.Dummy(new Vector2(0, 20 * ImGuiHelpers.GlobalScale));
-        if (wineUseNativeFFmpeg)
-        {
-            using (var child = ImRaii.Child("##wineFFmpegState", new Vector2(320 * ImGuiHelpers.GlobalScale, 55 * ImGuiHelpers.GlobalScale), true, ImGuiWindowFlags.NoScrollbar))
+            if (child)
             {
-                ImGui.TextWrapped($"FFmpeg daemon state: {(Plugin.FFmpegger.isFFmpegWineProcessRunning ? "Running" : "Stopped")}");
-                if (ImGui.Button("Start"))
+                ImGui.Dummy(new Vector2(0, 10 * ImGuiHelpers.GlobalScale));
+                ImGui.TextWrapped("FFmpeg Settings");
+                ImGui.Dummy(new Vector2(0, 10 * ImGuiHelpers.GlobalScale));
+
+                var wineUseNativeFFmpeg = Plugin.Config.WineUseNativeFFmpeg;
+                if (ImGui.Checkbox("##wineUseNativeFFmpeg", ref wineUseNativeFFmpeg))
                 {
-                    Plugin.FFmpegger.StartFFmpegWineProcess();
+                    Plugin.Config.WineUseNativeFFmpeg = wineUseNativeFFmpeg;
+                    if (wineUseNativeFFmpeg)
+                    {
+                      Plugin.FFmpegger.StartFFmpegWineProcess();
+                    }
+                    else
+                    {
+                      Plugin.FFmpegger.StopFFmpegWineProcess();
+                    }
+                    Plugin.Config.Save();
                 }
                 ImGui.SameLine();
-                if (ImGui.Button("Stop"))
+                ImGui.Text("Use native FFmpeg");
+                ImGui.Indent(16 * ImGuiHelpers.GlobalScale);
+                ImGui.Bullet();
+                ImGui.TextWrapped("Increases speed and prevents lag spikes on voices with effects (e.g. Dragons) and when using a playback speed other than 100.");
+                ImGui.Unindent(16 * ImGuiHelpers.GlobalScale);
+                
+                ImGui.Dummy(new Vector2(0, 20 * ImGuiHelpers.GlobalScale));
+                if (wineUseNativeFFmpeg)
                 {
-                    Plugin.FFmpegger.StopFFmpegWineProcess();
+                    using (var child2 = ImRaii.Child("##wineFFmpegState", new Vector2(345 * ImGuiHelpers.GlobalScale, 60 * ImGuiHelpers.GlobalScale), true, ImGuiWindowFlags.NoScrollbar))
+                    {
+                        if (child2)
+                        {
+                            ImGui.TextWrapped($"FFmpeg daemon state: {(Plugin.FFmpegger.isFFmpegWineProcessRunning ? "Running" : "Stopped")}");
+                            if (ImGui.Button("Start"))
+                            {
+                                Plugin.FFmpegger.StartFFmpegWineProcess();
+                            }
+                            ImGui.SameLine();
+                            if (ImGui.Button("Stop"))
+                            {
+                                Plugin.FFmpegger.StopFFmpegWineProcess();
+                            }
+                            ImGui.SameLine();
+                            if (ImGui.Button("Refresh"))
+                            {
+                                Plugin.FFmpegger.RefreshFFmpegWineProcessState();
+                            }
+                            ImGui.SameLine();
+                            if (ImGui.Button("Copy Start Command"))
+                            {
+                                ImGui.SetClipboardText($"/usr/bin/env bash -c '/usr/bin/env nohup /usr/bin/env bash \"{Plugin.FFmpegger.FFmpegWineScriptPath}\" {Plugin.FFmpegger.FFmpegWineProcessPort} >/dev/null 2>&1' &");
+                            }
+
+                            if (ImGui.IsItemHovered())
+                                using (ImRaii.Tooltip())
+                                    ImGui.TextUnformatted("Copies the command to start the ffmpeg-wine daemon manually. Run this in your native linux/macos console.");
+                        }
+                    }
+
+                    if (!Plugin.FFmpegger.isFFmpegWineProcessRunning)
+                    {
+                        if (Plugin.FFmpegger.IsWineDirty)
+                        {
+                          ImGui.TextWrapped("Warning: ffmpeg-wine might require wine to fully restart for registry changes to take effect.");
+                          ImGui.Dummy(new Vector2(0, 20 * ImGuiHelpers.GlobalScale));
+                        }
+                        using (var child2 = ImRaii.Child("##wineFFmpegTroubleshooting", new Vector2(345 * ImGuiHelpers.GlobalScale, 285 * ImGuiHelpers.GlobalScale), true, ImGuiWindowFlags.NoScrollbar))
+                        {
+                            if (child2)
+                            {
+                                ImGui.TextWrapped("If the FFmpeg daemon fails to start, check the following:");
+                                ImGui.Indent(4 * ImGuiHelpers.GlobalScale);
+                                ImGui.Bullet();
+                                ImGui.TextWrapped("'/usr/bin/env' exists");
+                                ImGui.Bullet();
+                                ImGui.TextWrapped("bash is installed system-wide as 'bash'");
+                                ImGui.Bullet();
+                                if (Plugin.FFmpegger.IsMac())
+                                {
+                                    ImGui.TextWrapped("netstat is installed system-wide as 'netstat'");
+                                }
+                                else
+                                {
+                                    ImGui.TextWrapped("ss is installed system-wide as 'ss'");
+                                }
+                                ImGui.Bullet();
+                                ImGui.TextWrapped("ffmpeg is installed system-wide as 'ffmpeg'");
+                                ImGui.Bullet();
+                                ImGui.TextWrapped("pgrep is installed system-wide as 'pgrep'");
+                                ImGui.Bullet();
+                                ImGui.TextWrapped("grep is installed system-wide as 'grep'");
+                                ImGui.Bullet();
+                                ImGui.TextWrapped("ncat is installed system-wide as 'ncat'");
+                                ImGui.Indent(16 * ImGuiHelpers.GlobalScale);
+                                ImGui.Bullet();
+                                ImGui.TextWrapped("Not 'netcat' nor 'nc'. ncat is usually part of the 'nmap' package");
+                                ImGui.Unindent(16 * ImGuiHelpers.GlobalScale);
+                                ImGui.Bullet();
+                                ImGui.TextWrapped("wc is installed system-wide as 'wc'");
+                                ImGui.Bullet();
+                                ImGui.TextWrapped($"port {Plugin.FFmpegger.FFmpegWineProcessPort} is not in use");
+                                ImGui.Unindent(4 * ImGuiHelpers.GlobalScale);
+                            }
+                        }
+                    }
                 }
             }
-
-            if (!Plugin.FFmpegger.isFFmpegWineProcessRunning)
-            {
-                ImGui.TextWrapped("If the FFmpeg daemon fails to start, check the following:");
-                ImGui.Indent(4 * ImGuiHelpers.GlobalScale);
-                ImGui.Bullet();
-                ImGui.TextWrapped("'/usr/bin/env' exists");
-                ImGui.Bullet();
-                ImGui.TextWrapped("bash is installed system-wide as 'bash'");
-                ImGui.Bullet();
-                ImGui.TextWrapped("ffmpeg is installed system-wide as 'ffmpeg'");
-                ImGui.Bullet();
-                ImGui.TextWrapped("netcat (BSD version) is installed system-wide as 'nc'");
-                ImGui.Bullet();
-                ImGui.TextWrapped("port 6914 is not in use");
-                ImGui.Unindent(4 * ImGuiHelpers.GlobalScale);
-            }
         }
+        ImGui.Indent(8 * ImGuiHelpers.GlobalScale);
     }
 
     private void Changelog()
